10b8eac82a2e9c9986186d260fdc04275ba47fb8
Lando <lando.schumpich@gmail.com>
2022-11-24 21:48:47 +0100

    Cleanup of consumer.py

--- src/consumer.py
+++ src/consumer.py
@@ -1,13 +1,15 @@
-from requests import get, put
+from requests import put
 from io import BytesIO
 from PIL import Image
 import base64
 import os
 import sys
 from dotenv import load_dotenv
+from torchworker import TorchWorker
 
 if not load_dotenv("./.env"):
     print("Consumer failed to load environment")
+
 
 def decode_image(img_str):
     # remove message fluff
@@ -17,12 +19,13 @@ def decode_image(img_str):
     # TODO: Fix image loading issue
     try:
         image = Image.open(buffer)
-    except e:
+    except:
         print("Failed to decode image string")
         print(type(img_str))
         print(img_str)
-        raise e
+        raise
     return image
+
 
 class Consumer(object):
     """
@@ -32,7 +35,7 @@ class Consumer(object):
 
     ip = os.getenv("SERVER_IP", "http://127.0.0.1")
     port = os.getenv("SERVER_PORT", "5000")
-    url = f"{ip}:{port}/" # localhost on port 5000
+    url = f"{ip}:{port}/"  # localhost on port 5000
 
     def __init__(self, url=None):
         if url is not None:
@@ -43,16 +46,16 @@ class Consumer(object):
 
     def generate_image(self, prompt, steps=10, seed=None):
         """
-        Generate an image with the given number of generation steps  
+        Generate an image with the given number of generation steps
         """
         args = {}
         if seed is None:
             seed = TorchWorker.generate_seed()
         args['seed'] = seed
         args['steps'] = steps
-        
         return put(self._prompt_url(prompt), json=args).json()
-        
+
+
 if __name__ == "__main__":
 
     args = sys.argv
@@ -64,7 +67,7 @@ if __name__ == "__main__":
 
     consumer = Consumer()
     result = consumer.generate_image(prompt, steps=20)
-    for k,v in result.items():
+    for k, v in result.items():
         if k == 'image':
             image = decode_image(v)
             filepath = f"./generated/{result['prompt'].replace(' ', '_')}.png"
@@ -73,4 +76,3 @@ if __name__ == "__main__":
             image.save(filepath)
         else:
             print(k, ": ", v)
-    
