8fb0cafc881b03256d94fbb948b4b3744a986cb5
Lando <lando.schumpich@gmail.com>
2023-01-08 15:10:45 +0100

    Add imageworker to handle images input by users

---
+++ src/imageworker.py
@@ -0,0 +1,41 @@
+from PIL import Image
+
+ALPHA_TOLERANCE = 10
+
+class ImageWorker():
+    image: Image = None
+    
+    def __init__(self, image):
+        self.image = image
+
+    def create_mask(self) -> Image:
+        """
+        Generates a mask from an image with transparency.
+        Stable diffusion inpainting wants a mask where black pixels
+        are kept and white pixels are re-drawn, so we transform the original
+        image pixels accordingly    
+        """
+        width, height = self.image.size
+        mask = self.image.copy()
+        data = mask.load()
+        for y in range(height):
+            for x in range(width):
+                color = data[x, y]
+                alpha = color[-1]
+                if alpha > ALPHA_TOLERANCE:
+                   color = (0, 0, 0, alpha)
+                else:
+                   color = (255, 255, 255, 255)
+                data[x, y] = color
+                
+        return mask
+        
+if __name__ == "__main__":
+    
+    img = Image.open("./images/debug_input.png")
+    worker = ImageWorker(img)
+    
+    mask = worker.create_mask()
+    
+    mask.save("./images/debug_mask_generated.png")    
+    
