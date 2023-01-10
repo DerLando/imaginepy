65b46d57f0672c5d4ed0b502e34b60fe9052b615
Lando <lando.schumpich@gmail.com>
2023-01-10 11:10:23 +0100

    Undo blur change

--- src/imageworker.py
+++ src/imageworker.py
@@ -52,8 +52,8 @@ class ImageWorker():
                 color = data[x, y]
                 alpha = color[-1]
                 if alpha > ALPHA_TOLERANCE:
-                   color = (0, 0, 0, 255)
-                   # color = (0, 0, 0, alpha)
+                   # color = (0, 0, 0, 255)
+                   color = (0, 0, 0, alpha)
                 else:
                    color = (255, 255, 255, 255)
                 data[x, y] = color
